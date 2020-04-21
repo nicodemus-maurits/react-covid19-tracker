4c195e06e29c4804776cc016874eb6f8be9c8fee
Nicodemus <nicodemus.maurits@gmail.com>
2020-04-21 14:55:08 +0200

    Init Chart component

--- src/api/index.js
+++ src/api/index.js
@@ -15,3 +15,15 @@ export const fetchData = async () => {
     };
   } catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await Axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+    return modifiedData;
+  } catch (error) {}
+};
